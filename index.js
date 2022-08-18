46a2ba4c87efc8f19782749d0d49a007fdafd01d
D-Micheni <dennis.micheni@student.moringaschool.com>
2022-08-18 07:52:45 +0300

    commit tree 2aab5afd35c130cade5635452a455675522268b9

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // code your solution here
+function saturdayFun(form='roller-skate') {
+    return `This Saturday, I want to ${form}!`
+   }
+   saturdayFun()
+   function mondayWork(plan='go to the office') {
+    return `This Monday, I will ${plan}.`
+   }
+   mondayWork('This Monday, I will work from home.')
+function wrapAdjective(adjective){
+   const specialChar=function(char){
+       return `You are ${adjective}${char}${adjective}!`
+   }
+   return specialChar
+}
+wrapAdjective('*')
